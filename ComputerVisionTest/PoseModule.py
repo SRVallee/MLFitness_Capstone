e2c788011682252d82b07d194bb8de2098dfa5bb
Reyes <wherismyboard@gmail.com>
2023-01-07 22:01:04 -0700

    movenet thunder model saved loacally

--- ComputerVisionTest/PoseModule.py
+++ ComputerVisionTest/PoseModule.py
@@ -56,16 +56,19 @@ def main():
     while True:
         #this sees if it can read the frame that it is given
         success, img = cap.read()
-        img = detector.findPose(img)
-        lmList = detector.findPosition(img)
-        #prints list of landmarks from 1 to 32 look at mediapipe diagram to know what landmark is which bodypart
-        print(lmList)
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)  # millisecond delays
+        if success:
+            img = detector.findPose(img)
+            lmList = detector.findPosition(img)
+            #prints list of landmarks from 1 to 32 look at mediapipe diagram to know what landmark is which bodypart
+            print(lmList)
+            cTime = time.time()
+            fps = 1 / (cTime - pTime)
+            pTime = cTime
+            cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+            cv2.imshow("Image", img)
+            cv2.waitKey(1)  # millisecond delays
+        else:
+            break
 
 if __name__ == "__main__":
     main()
