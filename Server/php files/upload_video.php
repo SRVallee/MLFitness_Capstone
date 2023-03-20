5f258ed3202cb8509105899eaed156faefbb7f49
Reyes <wherismyboard@gmail.com>
2023-03-19 22:42:31 -0600

    upload video test

--- Server/php files/upload_video.php
+++ Server/php files/upload_video.php
@@ -1,17 +1,28 @@
 <?php
-$target_dir = "../../../../videos";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "success";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
+if (isset($_FILES['video'])) {
+  $user_id = (int)$_POST["id"];
+  $apiKey = $_POST["apiKey"];
+  $trainer_id = (int)$_POST["trainer_id"];
+  $target_path = "../../../../videos" . basename($_FILES['video']['name']);
+
+  $conn = mysqli_connect("localhost", "root", "MLFitness@123", "ml_fitness"); //connect
+  $sql = "select * from user where (user_id = '".$user_id."' and api_key = '".$apiKey."')";
+  $res = mysqli_query($conn, $sql);
+  if(mysqli_num_rows($res) != 0){
+    $row = mysqli_fetch_assoc($res);
+
+    try {
+      if (move_uploaded_file($_FILES['video']['tmp_name'], $target_path)) {
+          echo "success";
+      } else {
+          echo "failed to move uploaded file";
+      }
+  } catch (Exception $e) {
+      echo "Caught exception: " . $e->getMessage();
   }
+  } else{
+    echo "Error uploading video(user).";
+  }
+} else {
+  echo "Video parameter not found.";
 }
-?>
