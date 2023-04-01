a32947823a52ecce34ac8e06bf33a2798d80e0c9
Reyes <wherismyboard@gmail.com>
2023-03-31 22:15:37 -0600

    going to profile from trainer fixed

--- Client/app/src/main/java/com/example/application/TrainerProfile.java
+++ Client/app/src/main/java/com/example/application/TrainerProfile.java
@@ -129,8 +129,15 @@ public class TrainerProfile extends AppCompatActivity {
                     }
                     case R.id.profile: {
                         //Already selected
-                        //Close drawer
-                        drawerLayout.closeDrawer(GravityCompat.START);
+                        if(SocketFunctions.user.isTrainer()){
+                            Intent i = new Intent(getApplicationContext(), TraineeProfile.class);
+                            i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                            startActivity(i);
+                            finish();
+                            break;
+                        }else {
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                        }
                         break;
                     }
                     case R.id.logout: {
