cd91dc770834dc65edb73ef0b9fc116350a7fec6
Reyes <wherismyboard@gmail.com>
2023-04-01 22:52:05 -0600

    unfriend and all users

--- Server/php files/add_relationship.php
+++ Server/php files/add_relationship.php
@@ -8,7 +8,7 @@ if($id == $id2){
     echo "can't add yourself";
 }else{
     $conn = mysqli_connect("localhost", "root", "FitnessPassword@123", "ml_fitness"); //connect
-    $sql = "SELECT user_id, api_key from user where (user_id = '".$id."' and api_key = '".$apiKey."');";
+    $sql = "SELECT user_id, api_key from user where (user_id = '".$id."' and api_key = '".$apiKey."') or user_id = ".$id2.";";
     $res = mysqli_query($conn, $sql);
     if(mysqli_num_rows($res) == 2){
         $row = mysqli_fetch_assoc($res);
@@ -16,9 +16,16 @@ if($id == $id2){
         $sql = "SELECT * FROM relationships WHERE (user_id = ".$id." and user_id_2 = ".$id2.") or (user_id = ".$id2." and user_id_2 = ".$id.");";
         $res = mysqli_query($conn, $sql);
         if(mysqli_num_rows($res) != 0){  // If relationship existed before
-            $sql = "UPDATE relationships SET end_date = ".date('Y-m-d').";";
-            mysqli_query($conn, $sql);
-            echo "success";
+            $row = mysqli_fetch_assoc($res);
+            if(is_null($row["end_date"])){
+                $sql = "UPDATE relationships SET end_date = ".date('Y-m-d').";";
+                mysqli_query($conn, $sql);
+                echo "success";
+            }else{
+                $sql = "UPDATE relationships SET end_date = null;";
+                mysqli_query($conn, $sql);
+                echo "success";
+            }
         }else{                          //else make new one
             $sql = "INSERT INTO relationships(user_id, user_id_2, log_location, training_relationship, start_date) values(".$id.", ".$id2.", 'none', ".$subscription.", ".date('Y-m-d').");";
             mysqli_query($conn, $sql);
---
+++ Server/php files/get_all_users.php
@@ -0,0 +1,19 @@
+<?php
+$id = $_POST['id'];
+$apiKey = $_POST['apiKey'];
+$conn = mysqli_connect("localhost", "root", "FitnessPassword@123", "ml_fitness"); //connect
+$sql = "SELECT user_id, api_key from user where (user_id = '".$id."' and api_key = '".$apiKey."');";
+$res = mysqli_query($conn, $sql);
+if(mysqli_num_rows($res) != 0){
+    $sql = "SELECT user_id, username, name, email from user;";
+    $res = mysqli_query($conn, $sql);
+    $array = array();
+    while($row = mysqli_fetch_assoc($res)){
+        $array[] = $row;
+    }
+    $result = array("status" => "success", "trainees" => $array);
+}else{
+    $result = array("status" => "Denied");
+}
+
+echo json_encode($result, JSON_PRETTY_PRINT);
