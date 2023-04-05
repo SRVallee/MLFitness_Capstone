f9959402c2d1978e4740f71cdcb2f0dac1e7a0df
JustinN9 <nortonj9@mymacewan.ca>
2023-04-05 00:28:02 -0600

    help

---
+++ Client/app/src/main/java/com/example/application/TrainerFeedback.java
@@ -0,0 +1,46 @@
+package com.example.application;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+
+import java.util.ArrayList;
+
+public class TrainerFeedback extends AppCompatActivity {
+
+    private EditText feetBack;
+
+    private TextView workoutTitle, reviewerName, feedback;
+
+    private VideoView videoFeedback;
+    private ArrayList<Workout> workouts = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer_feetback);
+
+        workoutTitle = findViewById(R.id.feetBackTitle);
+        reviewerName = findViewById(R.id.workout_feedback_by);
+        feedback = findViewById(R.id.workout_feedback_text);
+        videoFeedback = findViewById(R.id.workout_video);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Go to homepage
+        Intent i = new Intent(getApplicationContext(), TrainerHomePage.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(i);
+        finish();
+        super.onBackPressed();
+    }
+}
