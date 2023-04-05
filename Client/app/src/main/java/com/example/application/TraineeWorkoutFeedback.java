35d7975b9e3b06ee57d50b8099766bca8652da2d
Sera Vallee <seraRvallee@gmail.com>
2023-04-05 00:30:40 -0600

    trainee feedback backpress to home

--- Client/app/src/main/java/com/example/application/TraineeWorkoutFeedback.java
+++ Client/app/src/main/java/com/example/application/TraineeWorkoutFeedback.java
@@ -13,6 +13,7 @@ import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -147,4 +148,16 @@ public class TraineeWorkoutFeedback extends AppCompatActivity {
         };
         queue.add(stringRequest);
     }
+
+    @Override
+    public void onBackPressed() {
+        //Go to homepage
+        Intent i = new Intent(getApplicationContext(), TraineeHomePage.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(i);
+        finish();
+        super.onBackPressed();
+
+    }
+
 }
